88bc4f26baf580edc519f1c18fbc3137f64518a0
Piotr <p.skolorzynski@gmail.com>
2021-04-22 23:52:03 +0200

    used create-react-app. Added function to delete tasks

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import Todos from './Todos';
+
+// root component, container where todos are to be stored
+class App extends Component {
+  state = {
+    todos: [
+      //todo examples
+      {id: 1, content: 'drink a coffee'},
+      {id: 2, content: 'wash dishes'}
+    ]
+  }
+  //delete task
+ deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id // removes only clicked task
+    })
+    this.setState({
+      todos: todos //if the same names it is shorter to write just todos
+    })
+  }
+  render(){
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
