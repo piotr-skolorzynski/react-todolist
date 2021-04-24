7f49fc07bac893126e16bf78360284664d0c4da8
Piotr <p.skolorzynski@gmail.com>
2021-04-24 22:50:00 +0200

    AddForm.js file created. Added AddTodo container component. App component modified. Added method to add task to list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todos from './Todos';
+import AddTodo from './AddForm';
 
 // root component, container where todos are to be stored
 class App extends Component {
@@ -19,11 +20,20 @@ class App extends Component {
       todos: todos //if the same names it is shorter to write just todos
     })
   }
+
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
   render(){
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
